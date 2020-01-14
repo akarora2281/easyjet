fa155378d8bf25967ab7f831e5a4dc2362254a1b
amit <amitk@kainos.com>
2020-01-14 23:02:08 +0000

    Added unit test for GetAll Method and test failed because method is not implemented yet

--- EasyJet.Interview.Tests/GenericRepositoryTests.cs
+++ EasyJet.Interview.Tests/GenericRepositoryTests.cs
@@ -1,18 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace EasyJet.Interview.Tests
 {
     public class GenericRepositoryTests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+        private object expectedResult;
+
+        public GenericRepository<TestEmployee, int> _repository { get; private set; }
 
         [Test]
         public void Should_Returns_IEnumberable_When_GetAll_Method_Called()
         {
-            Assert.Fail();
+            _repository = new GenericRepository<TestEmployee, int>();
+            expectedResult = _repository.GetAll();
+
+            Assert.NotNull(expectedResult);
         }
     }
 }
---
+++ EasyJet.Interview.Tests/TestStoreableItem.cs
@@ -0,0 +1,10 @@
+﻿using EasyJet.Interview.Interface;
+
+namespace EasyJet.Interview.Tests
+{
+    public class TestEmployee : IStoreable<int>
+    {
+        public int Id { get; set; }
+        public string EmployeeName { get; set; }
+    }
+}
