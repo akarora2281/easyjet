a48cb69d5a28c97e49965c973b41586eb7930389
amit <amitk@kainos.com>
2020-01-14 23:16:22 +0000

    Added unit test to Add new item into repository and test failed as method not implemented yet.

--- EasyJet.Interview.Tests/GenericRepositoryTests.cs
+++ EasyJet.Interview.Tests/GenericRepositoryTests.cs
@@ -13,10 +13,29 @@ namespace EasyJet.Interview.Tests
         [Test]
         public void Should_Returns_IEnumberable_When_GetAll_Method_Called()
         {
+            //Arrange
             _repository = new GenericRepository<TestEmployee, int>();
+
+            // Act
             expectedResult = _repository.GetAll();
 
+            //Assert
             Assert.NotNull(expectedResult);
+        }
+
+        [Test]
+        public void Should_Save_NewItem_When_AddNewItem_Called_With_Valid_ItemData()
+        {
+            //Arrange
+            _repository = new GenericRepository<TestEmployee, int>();
+            var newEmployee = new TestEmployee { Id = 1, EmployeeName = "Emp1" };
+
+            // Act
+            _repository.Save(newEmployee);
+            expectedResult = _repository.GetAll();
+
+            //Assert
+            Assert.IsTrue(((IEnumerable<TestEmployee>)expectedResult).Contains(newEmployee));
         }
     }
 }
